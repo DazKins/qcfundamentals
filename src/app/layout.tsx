a7d2e8817dd81e966c29c7eabb072f71cee074e5
DazKins <david.atkins1998@gmail.com>
2024-05-12 19:52:29 +0800

    Skeleton website

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "QC Fundamentals",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
