da32370c1bb64f579a2d991b51c4f2d779cb212c
DazKins <david.atkins1998@gmail.com>
2024-05-14 18:44:18 +0800

    Re-order articles

--- src/article/articleDefinitions.ts
+++ src/article/articleDefinitions.ts
@@ -4,27 +4,23 @@ export type ArticleDefinition = {
   title: string;
   description: string;
   document: string;
-  date: Date;
 };
 
 export const ArticleDefinitions: ArticleDefinition[] = [
   {
-    title: "Multi-Qubit Computation",
-    description: "Performing quantum computation on multiple qubits",
-    document: "multi-qubit-computation",
-    date: new Date(9999, 4, 12),
+    title: "What is a Qubit?",
+    description: "An introduction to the fundamental unit of quantum computing",
+    document: "what-is-a-qubit",
   },
   {
     title: "Bra-ket notation",
     description: "Linear algebra without the matrices",
     document: "bra-ket-notation",
-    date: new Date(2024, 3, 3),
   },
   {
-    title: "What is a Qubit?",
-    description: "An introduction to the fundamental unit of quantum computing",
-    document: "what-is-a-qubit",
-    date: new Date(2024, 2, 2),
+    title: "Multi-Qubit Computation",
+    description: "Performing quantum computation on multiple qubits",
+    document: "multi-qubit-computation",
   },
 ];
 
--- src/components/article.tsx
+++ src/components/article.tsx
@@ -14,7 +14,6 @@ const Article = ({
       <a href="/">{"<"} Home</a>
       <h1>{articleDefinition.title}</h1>
       <h2 className="text-lightgrey">{articleDefinition.description}</h2>
-      <h3 className="text-darkgrey">{articleDefinition.date.toDateString()}</h3>
       {children}
     </div>
   );
