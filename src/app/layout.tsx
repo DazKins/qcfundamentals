860175dec1a83c124c184a73a84e68fa7328fe44
DazKins <david.atkins1998@gmail.com>
2024-07-09 23:11:40 +0800

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,29 +19,6 @@ export default function RootLayout({
     <html
       className={`bg-black text-white leading-7 tracking-wide ${normal.className}`}
     >
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </head>
       <body>
         <TopBar />
         <div className="w-full flex justify-center">
