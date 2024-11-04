76c03209bb4f6d432504ad269714e8261cb6aa16
DazKins <david.atkins1998@gmail.com>
2024-11-04 20:22:00 +0800

    Add open source details

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,6 +6,11 @@ const Page = () => {
         QCFundamentals is made by <a href="https://dazkins.com">DazKins</a>.
       </p>
       <p>
+        The project is fully open-sourced at{" "}
+        <a href="https://github.com/DazKins/qcfundamentals">GitHub</a>{" "}
+        where pull requests are welcome for all improvements/suggestions.
+      </p>
+      <p>
         If you have any suggestions or corrections please don&apos;t hesitate to
         reach out to me on <a href="https://x.com/dazkins_">X</a>.
       </p>
